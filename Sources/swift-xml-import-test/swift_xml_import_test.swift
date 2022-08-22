ac7c7373d5f37055575c6177d6f6e9d06cb00a9e
damien.morard <damien.morard@unige.ch>
2022-08-22 13:32:30 +0200

    Correct indentation

--- Sources/swift-xml-import-test/swift_xml_import_test.swift
+++ Sources/swift-xml-import-test/swift_xml_import_test.swift
@@ -41,15 +41,15 @@ class PnmlParser: NSObject, XMLParserDelegate {
 
 
 extension Bundle {
-    func getFileData(_ file: String) -> Data {
-        guard let url = self.url(forResource: file, withExtension: nil) else {
-            fatalError("Failed to locate \(file) in bundle")
-        }
-        
-        guard let data = try? Data(contentsOf: url) else {
-            fatalError("Failed to load \(file) in bundle")
-        }
-        
-        return data
+  func getFileData(_ file: String) -> Data {
+    guard let url = self.url(forResource: file, withExtension: nil) else {
+      fatalError("Failed to locate \(file) in bundle")
     }
+    
+    guard let data = try? Data(contentsOf: url) else {
+      fatalError("Failed to load \(file) in bundle")
+    }
+    
+    return data
+  }
 }
